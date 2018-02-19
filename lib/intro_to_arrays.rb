b5cfe85a8cdce455f4f9d3f568ac3a95149b1495
Chase <chaseit12@gmail.com>
2018-02-19 21:45:43 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,28 @@
+
+def instantiate_new_array
+  @my_new_array = []
+  @my_new_array
+end
+
+def array_with_two_elements
+  arr = [2,3]
+  arr
+end
+
+
+def first_element(arr)
+  arr[0]
+end
+
+def third_element(arr)
+  arr[2]
+end
+
+def last_element(arr)
+  arr[-1]
+end
+
+
+def first_element_with_array_methods(arr)
+    
+end
